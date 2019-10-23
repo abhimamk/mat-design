48888692cf18c0022a498a26bf790552b5a43350
User1 <user1@Skycliffitdomain.com>
2019-10-23 11:45:55 +0530

    clode button

--- src/app/menu/productadd/productadd.component.ts
+++ src/app/menu/productadd/productadd.component.ts
@@ -1,6 +1,7 @@
 import { GetservService } from './../../services/getserv.service';
 import { Component, OnInit } from '@angular/core';
 import { Product } from 'src/app/classes/product';
+import { MatDialogRef } from '@angular/material';
 
 @Component({
   selector: 'app-productadd',
@@ -9,12 +10,14 @@ import { Product } from 'src/app/classes/product';
 })
 export class ProductaddComponent implements OnInit {
   arr: Product[] = [];
-  constructor(private _data:GetservService) { }
+  constructor(private _data:GetservService, private dialogRef: MatDialogRef<ProductaddComponent>,) { }
 
   ngOnInit() {}
   onSaveProduct(f) {
     this._data.addProduct(f.value).subscribe((data: any) => {
+      this.dialogRef.close(f.value);
       console.log(data);
     });
   }
 }
+// this.dialogRef.close(this.form.value);
